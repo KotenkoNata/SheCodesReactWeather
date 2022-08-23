accee32c6dd7501a84b92de428f888c856692abc
KotenkoNata <kotenkonata8@gmail.com>
2022-08-23 14:48:23 +0300

    feat: add link to GitHub repository

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <p>
+        <a href={'https://github.com/KotenkoNata/SheCodesReactWeather'}>Open-source code</a>, by Nataliia Kotenko from <a href={'https://www.shecodes.io/'}>She Codes</a>
+      </p>
     </div>
   );
 }
